15221621209c4b5f08aa5cc38cd737524056c5e2
Vitaliy <hladon@meta.ua>
2019-10-28 19:30:21 +0200

    1

---
+++ src/main/java/Bot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.logging.Level;
+
+public class Bot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+//            log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
+    public String getBotUsername() {
+        return "BotName";
+    }
+
+    public String getBotToken() {
+        return "BotToken";
+    }
+}
