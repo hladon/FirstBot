deb9d450ea0178cdb7ff217c039861f616508dfe
Vitaliy <hladon@meta.ua>
2019-10-30 17:57:47 +0200

    1

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,36 +1,77 @@
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+
+import com.vdurmont.emoji.EmojiParser;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-@Service
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Bot extends TelegramLongPollingBot {
 
 
-    @Value("${bot.token")
-    private String botToken;
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText()+"I love Lera!");
+            SendMessage message = new SendMessage();// Create a SendMessage object with mandatory fields
+            message=generateMessage(update);
+
             try {
                 execute(message); // Call method to send the message
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
+        }
     }
 
+    //Checking commands
+    private static SendMessage generateMessage(Update update) {
+        String text = update.getMessage().getText();
+        SendMessage message = new SendMessage()
+                .setChatId(update.getMessage().getChatId())
+                .setText("My keyboard")
+                .setReplyMarkup(keybord1());
+        if (text.equals("key1")) {
+            message.setText(EmojiParser.parseToUnicode(":fire:"));
+        }
+        if (text.equals("key2")) {
+            message.setText(EmojiParser.parseToUnicode(":poop:"));
+        }
+        if (text.equals("key3")) {
+            message.setText(EmojiParser.parseToUnicode(":alien:"));
+        }
+        if (text.equals("key4")) {
+            message.setText(EmojiParser.parseToUnicode(":wolf:"));
+        }
+        return message;
+    }
+
+    //Creating keyboard
+    private static ReplyKeyboardMarkup keybord1() {
+        ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup();
+        List<KeyboardRow> buttons = new ArrayList<KeyboardRow>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("key1");
+        row.add("key2");
+        buttons.add(row);
+        row = new KeyboardRow();
+        row.add("key3");
+        row.add("key4");
+        buttons.add(row);
+        keyboard.setKeyboard(buttons);
+        return keyboard;
 
     }
+
     public String getBotUsername() {
-        return "Vitalii23Bot" ;
+        return "Vitalii23Bot";
     }
 
     public String getBotToken() {
-        return botToken;
+        return "";
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
@@ -10,14 +9,13 @@ public class Main {
     Bot bot;
 
     public static void main(String[] args) {
-//        ApiContextInitializer.init();
-//        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-//        try {
-//            telegramBotsApi.registerBot(new Bot());
-//        } catch (TelegramApiRequestException e) {
-//            e.printStackTrace();
-//        }
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println( bot.getBotToken());
     }
 }
